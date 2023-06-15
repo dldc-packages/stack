627a6c2de0f301faec64e64fc47b513065270f42
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2023-06-16 01:24:50 +0200

    Remove deno build

--- deno/constants.ts
+++
@@ -1,4 +0,0 @@
-export const INTERNAL = Symbol.for('INTERNAL');
-export const PROVIDER = Symbol.for('PROVIDER');
-export const PARENT = Symbol.for('PARENT');
-export const DEBUG = Symbol.for('DEBUG');
--- deno/mod.ts
+++
@@ -1,4 +0,0 @@
-export * from './Key.ts';
-export * from './MissingContextError.ts';
-export * from './Staack.ts';
-export * from './StaackCore.ts';
