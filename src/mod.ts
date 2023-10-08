5403c102156cc6ef92385ada3c7b908de43ba32d
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2023-10-08 19:32:47 +0200

    Fix exports

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,14 @@
 export { Key } from './Key';
-export type { IKeyBase, IKeyConsumer, IKeyProvider, TArgsBase, TKey, TKeyProviderFn, TVoidKey } from './Key';
+export type {
+  IKeyBase,
+  IKeyConsumer,
+  IKeyProvider,
+  TArgsBase,
+  TKey,
+  TKeyProviderFn,
+  TStringify,
+  TVoidKey,
+} from './Key';
 export { MissingContextError } from './MissingContextError';
 export { Stack } from './Stack';
 export { StackCore } from './StackCore';
